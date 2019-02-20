482a35a49e9063b1e9fcbb84c4ddca990f27f3fa
Richard Chen <richard.c.chen@gmail.com>
2019-02-19 21:15:40 -0500

    added seasons text and icons

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      lat: null, errorMessage: ''
-    }
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({lat: position.coords.latitude}),
-      (err) => {
-        this.setState({errorMessage: err.message})
-      }
-    )
-  }
+    state = {lat: null, errorMessage: ''}
+
 
   componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
@@ -29,7 +19,7 @@ class App extends React.Component {
       }
 
       if(!this.state.errorMessage && this.state.lat){
-        return <div>Latitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat}/>
       }
 
       return <div>Loading!</div>
