20ea4058070e511eec241dc5ac4201defbbec114
Richard Chen <richard.c.chen@gmail.com>
2019-02-20 18:50:32 -0500

    added additional css styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css'
 import React from 'react'
 
 const seasonConfig = {
@@ -26,10 +27,11 @@ const SeasonDisplay = (props) => {
     const {text, iconName} = seasonConfig[season]
 
     return (
-      <div>
-        <i className={`massive ${seasonConfig[season].iconName} icon`} />
+
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${seasonConfig[season].iconName} icon`} />
         <h1>{text}</h1>
-        <i className={`massive ${iconName} icon`} />
+        <i className={`icon-right massive ${iconName} icon`} />
       </div>
     )
 }
