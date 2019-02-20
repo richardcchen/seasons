67cac7e02bdd3001b1a3f0437b325b4f2d57a6ce
Richard Chen <richard.c.chen@gmail.com>
2019-02-19 20:43:31 -0500

    added componentDidMount

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ class App extends React.Component {
     )
   }
 
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+    )
+  }
+
   render(){
       if (this.state.errorMessage && !this.state.lat){
         return <div>Error: {this.state.errorMessage}</div>
